6340b69f328fc1f4e0c210e198dc01971339aeb2
Danillo Ferreira <nilloferreiira@gmail.com>
2025-11-04 18:39:43 -0300

    notification service

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -7,19 +7,58 @@ class NotificationService {
 
   bool get isInitialized => _isInitialized;
 
-  //Initialize
+  // Initialize
   Future<void> initNotifications() async {
     if (_isInitialized) return; // prevent initialization
 
-    // // android init settings
-    // const initSettingsAndroid = AndroidInitializationSettings(
-    //   '@mipmap/ic_launcher',
-    // );
-    // // ios init settings
-    // const initSettingsIOS = DarwinInitializationSettings(
-    //   resquertAlertPermission: true,
-    // )
+    // android init settings
+    const initSettingsAndroid = AndroidInitializationSettings(
+      '@mipmap/ic_launcher',
+    );
+    // ios init settings
+    const initSettingsIOS = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+
+    // init setup
+
+    const initSettings = InitializationSettings(
+      android: initSettingsAndroid,
+      iOS: initSettingsIOS,
+    );
+
+    // initialize the plugin
+    await notificationPlugin.initialize(initSettings);
   }
 
-  //Notifications Detail setup
+  // Notifications Detail setup
+
+  NotificationDetails notificationDetails() {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'daily_channel_id',
+        'Daily Notificaitions',
+        channelDescription: 'Daily Notification Channel',
+        importance: Importance.max,
+        priority: Priority.high,
+      ),
+      iOS: DarwinNotificationDetails(),
+    );
+  }
+
+  // Show notification method
+  Future<void> showNotification({
+    int id = 0,
+    String? title,
+    String? body,
+  }) async {
+    return notificationPlugin.show(
+      id,
+      title,
+      body,
+      const NotificationDetails(),
+    );
+  }
 }
